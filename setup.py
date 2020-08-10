4b55563d718a6d5184f2096eea9069f6e47acb4e
Erfan Nourbakhsh <erfanxyz@gmail.com>
2020-08-10 01:05:04 -0700

    Name as a variable

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ package_name = 'skylink'
 lib_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), package_name)
 exec(open(os.path.join(lib_path, 'version.py')).read()) # it reads __version__
 
-setup(name='skylink',
+setup(name=package_name,
       version=__version__,
       description='Using KDTree search and graphs to match sky catalogs.',
       url='https://github.com/skylink',
